ae1c0b0e4e9f19efcd0a49187c5c1d89fe49783e
JLAndrade <JLAndrade@users.noreply.github.com>
2017-10-20 12:21:33 -0500

    Initial commit

---
+++ database/migrations/2017_10_20_170500_add_columns_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->string('ocupacion');
+            $table->integer('edad')->nullable();
+            $table->integer('activo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('ocupacion');
+            $table->dropColumn('edad');
+            $table->dropColumn('activo');
+        });
+    }
+}
